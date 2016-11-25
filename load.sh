dfaff9b7965614f725f3163b402724f47a31fb64
KAUSHIK PADMANABHAN <kpdare28@gmail.com>
2016-11-25 10:52:49 -0500

    Add files via upload

---
+++ load.sh
@@ -0,0 +1,19 @@
+#export LD_LIBRARY_PATH=/usr/local/mysql/lib/mysql/
+DBNAME=$2
+WH=$3
+HOST=$1
+STEP=400
+
+./tpcc_load -h $HOST -d $DBNAME -u root -p "mysql" -w $WH -l 1 -m 1 -n $WH >> 1.out &
+
+x=1
+
+while [ $x -le $WH ]
+do
+ echo $x $(( $x + $STEP - 1 ))
+./tpcc_load -h $HOST -d $DBNAME -u root -p "mysql" -w $WH -l 2 -m $x -n $(( $x + $STEP - 1 ))  >> 2_$x.out &
+./tpcc_load -h $HOST -d $DBNAME -u root -p "mysql" -w $WH -l 3 -m $x -n $(( $x + $STEP - 1 ))  >> 3_$x.out &
+./tpcc_load -h $HOST -d $DBNAME -u root -p "mysql" -w $WH -l 4 -m $x -n $(( $x + $STEP - 1 ))  >> 4_$x.out &
+ x=$(( $x + $STEP ))
+done
+
